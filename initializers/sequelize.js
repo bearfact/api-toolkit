5dd78b894d9b303670f1a743915e688c3be3fd36
Joseph Crone <jcrone@Josephs-MacBook-Pro.local>
2021-07-25 11:15:49 -0600

    adds option support for sequelize

--- initializers/sequelize.js
+++ initializers/sequelize.js
@@ -1,12 +1,15 @@
+const hash = require('object-hash');
+
 const connections = {};
 let sequelize;
-const sequelizeConnection = (connectionString) => {
-	if (!connections[connectionString]) {
+const sequelizeConnection = (connectionString, options) => {
+	const key = connectionString + hash(options);
+	if (!connections[key]) {
 		sequelize = require('sequelize');
 		const { Sequelize } = sequelize;
-		connections[connectionString] = new Sequelize(connectionString);
+		connections[key] = new Sequelize(connectionString, options);
 	}
-	return { db: connections[connectionString], sequelize }
+	return { db: connections[key], sequelize }
 }
 
 module.exports = sequelizeConnection;

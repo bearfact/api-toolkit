5c5459e9281c982e37119eea2f1d3bc1480f9f68
JD Crone <jdcrone@protonmail.com>
2021-09-27 11:57:46 -0600

    add sql logging as env variable

--- initializers/sequelize.js
+++ initializers/sequelize.js
@@ -1,15 +1,16 @@
-const hash = require('object-hash');
+const hash = require("object-hash");
 
 const connections = {};
 let sequelize;
 const sequelizeConnection = (connectionString, options = {}) => {
+	options = { logging: process.env.SQL_LOGGING === "true", ...options };
 	const key = connectionString + hash(options);
 	if (!connections[key]) {
-		sequelize = require('sequelize');
+		sequelize = require("sequelize");
 		const { Sequelize } = sequelize;
 		connections[key] = new Sequelize(connectionString, options);
 	}
-	return { db: connections[key], sequelize }
-}
+	return { db: connections[key], sequelize };
+};
 
 module.exports = sequelizeConnection;
